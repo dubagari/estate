b0343fe6ed9da93ecc2822e499ce44bb1cf59d6f
Abubakar Abdullahi Dubagari <superango93@gmail.com>
2025-03-25 17:20:05 +0100

    connect to DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("server is connected MONGODB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
