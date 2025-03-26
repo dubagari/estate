bee8e5b004d60552355a5e9de1e72e31c53f25ef
Abubakar Abdullahi Dubagari <superango93@gmail.com>
2025-03-26 12:06:22 +0100

    create middleware to handle posible error

--- api/index.js
+++ api/index.js
@@ -20,7 +20,18 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/user", userRoute);
+
 app.use("/api/auth", authRoute);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Enternal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 app.listen(3000, () => {
   console.log("server is running at port 3000");
