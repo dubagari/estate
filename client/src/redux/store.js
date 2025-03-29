d2a069331aad752e5b9053c09987022c0b1b51b7
Abubakar Abdullahi Dubagari <superango93@gmail.com>
2025-03-29 23:48:51 +0100

    add redux persis

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,16 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/userSlice";
 
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfiq = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfiq, rootReducer);
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
 
-// // Infer the `RootState` and `AppDispatch` types from the store itself
-// export type RootState = ReturnType<typeof store.getState>
-// // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-// export type AppDispatch = typeof store.dispatch
+export const persistor = persistStore(store);
