1c29a758dd7017d760a2ea4c2dd7265517ea175d
Abubakar Abdullahi Dubagari <superango93@gmail.com>
2025-06-11 05:55:52 +0100

    sign up fix

--- api/controller/auth.controllers.js
+++ api/controller/auth.controllers.js
@@ -7,6 +7,11 @@ export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hassPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hassPassword });
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({ message: "Email has already been taken" });
+  }
   try {
     await newUser.save();
     res.status(201).json("user created succesefully");
